f4b525a3612e9c868d8a70a49de9c4e0d595404c
Kdy0115 <k31m03.1020@gmail.com>
2021-09-14 18:15:13 +0900

    conversionファイルの作成　.gitignoreファイルの編集

--- controllers/cvt/conversion.py
+++ controllers/cvt/conversion.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import configparser
 import os
+import json
 import csv
+import datetime
 import glob
-from inc.cons import *
+from controllers.cvt.inc.cons import FLOOR5_COLUMN_NAME
+# from inc.cons import FLOOR5_COLUMN_NAME
 
 config_ini = configparser.ConfigParser()
 config_ini.read('config/config.ini', encoding='utf-8')
@@ -17,18 +20,18 @@ config_layout     = config_ini["LAYOUT"]
 class DataSet():
     def __init__(self, config_bems: str, config_control: str, config_layout: str, config_simulation: str,) -> None:
 
-        self.bems_data = {
-            "file_path" : config_bems["BEMS_file_path"],
-            "sheet_name": config_bems["Excel_sheet_name"],
-        }
+        self.bems_file_path = config_bems["BEMS_file_path"]
         
         self.control_data_folder_path = config_control["Control_file_path"]
-        self.control_data = []
 
         self.simulation_step = config_simulation["Simulation_time"]
+        self.laytout_file_path = config_layout["Lyaout_floor_file_path"]
         self.output_folder = config_simulation["Output_folder_path"]
         
         self.floors = [3,4,5]
+        self.init_bems_data = []
+        self.control_data = []
+        self.layout_data = []
 
     def create_output_folder(self) -> None:
         """ Create a folder for output file（結果出力ファイル用のフォルダを作成）
@@ -55,21 +58,33 @@ class DataSet():
 
             self.control_data.append(control_data_dic)
 
+    def import_all_layout_data(self):
+        f = open(self.laytout_file_path)
+        self.layout_data = json.load(f)
+        f.close()
+        
 
-    def _format_bems_data(self) -> dict:
-        df = pd.read_excel(self.bems_data["file_path"],sheet_name=self.bems_data["sheet_name"],header=7)
-        df_recent_bems = df.tail(1)
-        start_time = df_recent_bems["信号名称"].values[0]
-        bems_dict_list = []
+
+    def _import_bems_data(self) -> None:
+        csv_file = open(self.bems_file_path, "r", encoding="shift-jis")
+        f = csv.reader(csv_file)
+        header = next(f)
+        data = next(f)
+        self.start_time = data[0]
         for floor in self.floors:
-            df_recent_bems = df_recent_bems.loc[:,df_recent_bems.columns.str.contains('信号名称|外気温|吸込温度')].rename(columns=FLOOR5_COLUMN_NAME)
-            dict_bems = {key:value for key,value in zip(df_recent_bems.columns,df_recent_bems.iloc[0].values)}
-            dict_bems["floor"] = floor
-            bems_dict_list.append(dict_bems)
+            init_bems_data = {}
+            for key,value in zip(header,data):
+                if key == "信号名称":
+                    key = "時間"
+                init_bems_data[key] = value
+            init_bems_data["floor"] = floor
+            self.init_bems_data.append(init_bems_data)
 
-        return bems_dict_list,start_time
-
-    def sync_control_data(self,start_time) -> dict:
+    def _sync_control_data(self) -> dict:
+        
+        tdatetime = datetime.datetime.strptime(self.start_time.replace("/","-"), '%Y-%m-%d %H:%M')
+        start_time = tdatetime - datetime.timedelta(minutes=1)
+        
         if start_time.hour < 10:
             hour = "0{}".format(start_time.hour)
         else:
@@ -89,20 +104,24 @@ class DataSet():
     def integrate_files(self) -> None:
         self.post_data = {
             "simulation_step" :self.simulation_step,
-            "simulation_data" :[]
+            "simulation_data" :[],
+            "output_folder"   :self.output_folder,
         }
-        bems_list,start_time = self._format_bems_data()
+        self._import_bems_data()
         self.import_all_control_data()
-        self.sync_control_data(start_time)
+        self._sync_control_data()
+        self.import_all_layout_data()
 
-        for item in self.control_data:
-            for one in bems_list:
-                if item["floor"] == one["floor"]:
-                    one_floor_data = {
-                        "init_bems": one,
-                        "control_plan": item["control_data"],
-                    }
-                    self.post_data["simulation_data"].append(one_floor_data)
+        for l in self.control_data:
+            for m in self.layout_data:
+                for n in self.init_bems_data:
+                    if (l["floor"] == m["floor"]) and (m["floor"] == n["floor"]):
+                        one_post_data = {
+                            "init_bems_data": n,
+                            "control_data"  : l["control_data"],
+                            "layout_data"   : m
+                        }
+                    self.post_data["simulation_data"].append(one_post_data)
 
 dataset = DataSet(config_bems, config_control, config_layout, config_simulation)
 dataset.integrate_files()
