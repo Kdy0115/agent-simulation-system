4a592aa9c95c9e6c2ce8b5f63edae066e0cd7892
Kdy0115 <k31m03.1020@gmail.com>
2021-09-16 13:19:49 +0900

    実行ファイルの作成

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from controllers.sml.simulation import SimulationControl
 from controllers.cvt.conversion import dataset
 
 simulation = SimulationControl(dataset.post_data)
+# simulation.run_all_simulations()
+simulation.run_all_simulations_multi_process()
 
-#simulation.run_all_simulations()
-
-print(dataset.post_data)
