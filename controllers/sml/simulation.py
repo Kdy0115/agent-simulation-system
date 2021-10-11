e2f953c14a65c256f238e6ccd49206326db7e4be
Kdy0115 <k31m03.1020@gmail.com>
2021-10-12 02:46:25 +0900

    update heat source data

--- controllers/sml/simulation.py
+++ controllers/sml/simulation.py
@@ -2,26 +2,29 @@ import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 from controllers.sml.model import *
-from controllers.cvt.conversion import dataset
+# from controllers.cvt.conversion import dataset
 from controllers.sml.extension.visualization.canvas_grid_visualization_extension import CanvasGrid_3d
 from tqdm import tqdm
 import json
 import math
 import time
-from multiprocessing import Process
-import multiprocessing
+#from multiprocessing import Pool,freeze_support, RLock
+import multiprocessing as mp
 
 class SimulationControl():
 
     def __init__(self,post_data):
         self.simulation_step = int(post_data["simulation_step"]) * 60
+        # self.simulation_step = 1
         self.models_floor_dic = {}
         self.dataset = post_data["simulation_data"]
         self.output_folder = post_data["output_folder"]
 
         for data in self.dataset:
-            model = HeatModel(data["init_bems_data"]["floor"], self.simulation_step, data["init_bems_data"], data["control_data"],data["layout_data"])
+            model = HeatModel(data["init_bems_data"]["floor"], self.simulation_step, data["init_bems_data"], data["control_data"],data["layout_data"],data["source_data"])
             self.models_floor_dic[data["init_bems_data"]["floor"]] = model
+        # print(model.grid._agent_to_index)
+        # exit()
 
     def _str_simulation_state(self):
         print("Simulation starts")
@@ -36,12 +39,15 @@ class SimulationControl():
             fw = open('{0}/result{1}.json'.format(self.output_folder,result[0]),'w')
             json.dump(result[1],fw,indent=4)
 
-    def run_simulation(self,key,model):
-        for i in tqdm(range(self.simulation_step)):
+    def run_simulation(self,key,model,i):
+        info = f'プロセス#{i:>2} '
+        # for _ in tqdm(range(self.simulation_step), desc=info, position=i):
+        for _ in tqdm(range(self.simulation_step), desc=info,position=i):
             if model.terminate:
                 break
             else:
                 model.step()
+
         result = (key,model.spaces_agents_list)
         return result
 
@@ -61,11 +67,15 @@ class SimulationControl():
     def run_all_simulations_multi_process(self):
         start = time.time()
         self._str_simulation_state()
-        with multiprocessing.Pool(processes=6) as pool:
-            args = list(zip(self.models_floor_dic.keys(),self.models_floor_dic.values()))
-            self.output_data = pool.starmap(self.run_simulation, args)
+        args = list(zip(self.models_floor_dic.keys(),self.models_floor_dic.values(),range(3)))
+        L = len(args)
         
-        self.output_agent_json()
+        with mp.Pool() as pool:
+            self.output_data = pool.starmap(self.run_simulation, args)
+        print("\n" * L)
+        
         elapsed_time = time.time() - start
         print("Simulation finished!")
         print("Simulation time:{}".format(int(elapsed_time)) + "[sec]")
+
+        return self.output_data
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from controllers.sml.simulation import SimulationControl
-from controllers.cvt.conversion import dataset
+from controllers.cvt.conversion import *
+from views.dynamic_output import *
+import time
+#from multiprocessing import Pool,freeze_support, RLock
+import multiprocessing as mp
+from tqdm import tqdm
+
+dataset = DataSet(config_bems, config_control, config_layout, config_simulation)
+dataset.integrate_files()
+
 
 simulation = SimulationControl(dataset.post_data)
 # simulation.run_all_simulations()
-simulation.run_all_simulations_multi_process()
-
+result = simulation.run_all_simulations_multi_process()
+dataset.output_data(result)
