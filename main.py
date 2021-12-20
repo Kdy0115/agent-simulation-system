b0fc1a3799138676826b01e2d0f7fb61b0e61b6c
Kdy0115 <k31m03.1020@gmail.com>
2021-12-20 20:14:24 +0900

    edit files

--- main.py
+++ main.py
@@ -28,7 +28,6 @@ config_layout     = config_ini["LAYOUT"]
 dataset = DataSet(config_bems, config_control, config_layout, config_simulation)
 # 必要ファイルのデータ統合
 simulation_setting_data = dataset.integrate_files()
-
 simulation = SimulationControl(simulation_setting_data,dataset)
 
 # マルチプロセス処理で実行
@@ -41,3 +40,32 @@ if dataset.mp == True:
 elif dataset.mp == False:
     result = simulation.run_all_simulations()
     dataset.output_data(result)
+
+##########################################################
+#サーバー呼び出し用
+##########################################################
+# def main():
+#     # 設定ファイルの内容を取得
+#     config_ini = configparser.ConfigParser()
+#     config_ini.read('config/config.ini', encoding='utf-8')
+    
+#     config_bems       = config_ini["BEMS"]
+#     config_control    = config_ini["CONTROL"]
+#     config_simulation = config_ini["SIMULATION"]
+#     config_layout     = config_ini["LAYOUT"]
+    
+#     # DataSetクラスからインスタンスの生成(cvt内のファイルから参照)
+#     dataset = DataSet(config_bems, config_control, config_layout, config_simulation)
+#     # 必要ファイルのデータ統合
+#     simulation_setting_data = dataset.integrate_files()
+#     simulation = SimulationControl(simulation_setting_data,dataset)
+#     # マルチプロセス処理で実行
+#     if dataset.mp == True:
+#         if __name__ == '__main__':
+#             mp.freeze_support()
+#             result = simulation.run_all_simulations_multi_process()
+#             dataset.output_data(result)
+#     # 逐次処理で実行
+#     elif dataset.mp == False:
+#         result = simulation.run_all_simulations()
+#         dataset.output_data(result)
